bd2a7c620cbe304e7ec531ac4a94fa4ef5fb5da8
wenleyan1 <wenleyang@yeah.net>
2023-06-16 19:16:30 -0400

    fixed inference code for plotting and modified training/fine tuning hyperparameters

--- inference.py
+++ inference.py
@@ -6,7 +6,10 @@ import time
 import datetime
 import glob
 import json
+import logging
+from PIL import Image, ImageDraw
 
+logging.basicConfig(level=logging.ERROR)
 
 def yolo_predict_to_coco(results, image_path, image_id):
     """Converts YOLOv8 results to COCO format."""
@@ -15,9 +18,9 @@ def yolo_predict_to_coco(results, image_path, image_id):
     for i in range(len(results.boxes.cls)):
         this_coco = {}
         this_coco["image_id"] = image_id
-        this_coco["category_id"] = results.boxes.cls[i]
-        this_coco["bbox"] = results.boxes.xywh[i]
-        this_coco["score"] = results.boxes.conf[i]
+        this_coco["category_id"] = int(results.boxes.cls[i].cpu())
+        this_coco["bbox"] = results.boxes.xywh[i].cpu().numpy().tolist()
+        this_coco["score"] = results.boxes.conf[i].cpu().numpy().tolist()
         this_coco['image_path'] = image_path
         # this_coco['category_all'] = {0: "Cranes", 1: "Excavators", 2: "Bulldozers", 3: "Scrapers", 4: "Trucks", 5:"Workers"}
         coco.append(this_coco)
@@ -36,12 +39,12 @@ def replace_human_bbox(general_result, human_result):
 
 if __name__ == '__main__':
     start = time.time()
-
+    print('loading model...')
     os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     test_folder = r'Y:\datasets\Datathoni3CE2023\train\images'
-    output_folder = r'Y:\datasets\Datathoni3CE2023\train\labels'
+    output_folder = r'Y:\datasets\Datathoni3CE2023\temp_output'
     model_folder = r'runs\detect\best_storage'
     model = YOLO(os.path.join(model_folder, "YOLOv8x_best_gunwoo_20230614.pt"))
     model_human = YOLO(os.path.join(model_folder, "YOLOv8x_best_human_85.pt"))
@@ -61,23 +64,24 @@ if __name__ == '__main__':
         for file in files:
             if file.endswith(".jpg") or file.endswith(".png") or file.endswith(".jpeg"):
                 image_id += 1
-                if image_id < 5:
-                    visualise = True
-                else:
-                    visualise = False
                 print(f"Processing {image_id} / {count}: {file}", end='\r')
                 image_path = os.path.join(root, file)
                 # image_path starting from base_folder
                 relative_image_path = os.path.relpath(image_path, test_folder)
                 image = cv2.imread(image_path)
-                results = model.predict(source=image, save=visualise)
-                results_human = model_human.predict(source=image, save=visualise)
+                results = model.predict(source=image, verbose=False, imgsz=224)
+                results_human = model_human.predict(source=image, verbose=False, imgsz=640)
                 general_result = yolo_predict_to_coco(results, relative_image_path, image_id)
                 human_result = yolo_predict_to_coco(results_human, relative_image_path, image_id)
                 merged_result = replace_human_bbox(general_result, human_result)
                 coco_general.extend(general_result)
                 coco_human.extend(human_result)
                 coco_merged.extend(merged_result)
+                if image_id % 25 == 1 and len(human_result) > 0:
+                    display = Image.fromarray(cv2.cvtColor(results[0].plot(), cv2.COLOR_RGB2BGR))
+                    display.save(os.path.join(output_folder, f"{image_id}.jpg"))
+                    display_human = Image.fromarray(cv2.cvtColor(results_human[0].plot(), cv2.COLOR_RGB2BGR))
+                    display_human.save(os.path.join(output_folder, f"{image_id}_human.jpg"))
 
 
     end = time.time()
--- train.py
+++ train.py
@@ -6,16 +6,15 @@ import os
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-model = YOLO("yolov8x.pt")
-# model = YOLO("./runs/detect/train16/weights/best.pt")
-
-
-# # Use the model
-# results = model.train(data=".././datasets/Datathon2023/data.yaml", epochs=500, imgsz=224,
-#                       optimizer="Adam", seed=0, lr0=1e-3)  # train the model
-# metrics = model.val()  # evaluate model performance on the validation set
+# model = YOLO("yolov8x.pt")
+model = YOLO("./runs/detect/train10/weights/last.pt")
 
 # Use the model
-model.train(data="DatathonHumanPretrain.yaml", epochs=100, batch=10, workers=0, device='cuda', optimizer="Adam", imgsz=640)     # train the model
-metrics = model.val()  # evaluate model performance on the validation set
+model.train(data="DatathonHumanPretrain.yaml", epochs=100, lr0=0.0025, lrf=0.0025, warmup_epochs=0, batch=10, workers=0, device='cuda', optimizer="Adam", imgsz=640)     # train the model
+metrics = model.val()
+metrics_custom = model.val(data="DatathonHumanTrain.yaml", workers=0, device='cuda')  # evaluate model performance on the validation set
 path = model.export(format="onnx")  # export the model to ONNX format
+
+# fine tuning
+model.train(data="DatathonHumanTrain.yaml", epochs=200, batch=10, workers=0, device='cuda', optimizer="Adam", imgsz=640)     # train the model
+metrics_custom = model.val()  # evaluate model performance on the validation set
